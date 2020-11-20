78f5cdfe107438e871848b240bc68933ec4c1196
Aymane <danielayman@gmail.com>
2020-11-20 15:52:50 +0100

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Aymane DANIEL on 20/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
